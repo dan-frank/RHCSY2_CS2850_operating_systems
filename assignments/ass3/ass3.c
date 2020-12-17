4c3751c083083efa7fb3118ce76a16282be3231f
dan-frank <dan.frank.lucas@gmail.com>
2020-12-17 16:58:29 +0000

    Refactored: pthreads (2 threads)
    Updated pthread_join position to run both threads at the same time.

--- assignments/ass3/ass3.c
+++ assignments/ass3/ass3.c
@@ -63,8 +63,8 @@ int main() {
 
     pthread_t t1;
     pthread_create(&t1, NULL, (void *) pushIntegers, (struct list *) l);
-    pthread_join(t1, NULL);
     pthread_create(&t1, NULL, (void *) pullIntegers, (struct list *) l);
+    pthread_join(t1, NULL);
     pthread_join(t1, NULL);
 
     freeList(l);
